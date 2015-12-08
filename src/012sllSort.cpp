e53a61ff006301481ec73f672dec3763754d2020
srikeerthan <srikeerthanreddy@gmail.com>
2015-12-08 23:13:17 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -23,4 +23,24 @@ struct node {
 
 void sll_012_sort(struct node *head){
 	
+	int count[3] = { 0, 0, 0 }; 
+	struct node *ptr = head;
+	while (ptr != NULL)
+	{
+		count[ptr->data] += 1;
+		ptr = ptr->next;
+	}
+	int i = 0;
+	ptr = head;
+	while (ptr != NULL)
+	{
+		if (count[i] == 0)
+			++i;
+		else
+		{
+			ptr->data = i;
+			--count[i];
+			ptr = ptr->next;
+		}
+	}
 }
